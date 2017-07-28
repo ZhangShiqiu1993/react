594969b30d60635bc8942961aa39ce7a4310d2b1
zhangshiqiu <zhangshiqiu1993@126.com>
2017-07-27 22:16:39 -0700

    pass data with props

---
+++ contacts/src/ListContacts.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+
+class ListContacts extends Component {
+    render() {
+        console.log("this.props", this.props)
+        return (
+            <ol className='contact-list'>
+
+            </ol>
+        )
+    }
+}
+
+export default ListContacts
