1e6e838b04f723819ee30d88930c84b050fd4835
zhangshiqiu <zhangshiqiu1993@126.com>
2017-07-31 00:21:08 -0700

    remove data from db

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -4,31 +4,33 @@ import * as ContactsAPI from './utils/ContactsAPI'
 
 
 class App extends Component {
-  state = {
-      contacts : []
-  };
+    state = {
+        contacts : []
+    };
 
-  componentDidMount(){
-      ContactsAPI.getAll().then((contacts)=>{
-          this.setState({contacts})
-      })
-  }
+    componentDidMount(){
+        ContactsAPI.getAll().then((contacts)=>{
+            this.setState({contacts})
+        })
+    }
 
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
-  }
+    removeContact = (contact) => {
+        this.setState((state) => ({
+            contacts: state.contacts.filter((c) => c.id !== contact.id)
+        }))
 
-  render() {
-    return (
-      <div>
-        <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}/>
-      </div>
-    )
-  }
+        ContactsAPI.remove(contact)
+    }
+
+    render() {
+        return (
+            <div>
+                <ListContacts
+                    onDeleteContact={this.removeContact}
+                    contacts={this.state.contacts}/>
+            </div>
+        )
+    }
 }
 
 export default App;
