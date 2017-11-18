8fcd9b7dce2270f16098120dcbfc7be7c1bf9195
zhangshiqiu <zhangshiqiu1993@126.com>
2017-11-17 21:42:07 -0800

    contacts app

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
+class ContactList extends Component {
+    render() {
+        const people = this.props.contacts
 
-    return <ol>
-      {
-        people.map(person => (
-          <li key={person.name}>{person.name}</li>
-        ))
-      }
-    </ol>
-  }
+        return <ol>
+            {people.map(person => (
+                <li key={person.name}>{person.name}</li>
+            ))}
+        </ol>
+    }
 }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList contacts={[
-                     { name: 'Michael' },
-                    { name: 'Ryan' },
-                    { name: 'Tyler' }
-                  ]}/>
-                <ContactList contacts={[
-                  { name: 'Amanda' },
-                  { name: 'Richard' },
-                 { name: 'Geoff' }
-                ]}/>
+          <ContactList contacts={[
+              {name: 'Micheal'},
+              {name: 'Ryan'},
+              {name: 'Tyler'}
+          ]}/>
+          <ContactList contacts={[
+              {name: 'Amanda'},
+              {name: 'Richard'},
+              {name: 'Geoff'}
+          ]}/>
       </div>
     );
   }
