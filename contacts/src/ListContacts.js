8df4ad58df606d84c6e81858067067ffa00bc031
zhangshiqiu <zhangshiqiu1993@126.com>
2017-11-20 12:44:13 -0800

    clear query function

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -7,20 +7,23 @@ class ListContacts extends Component{
     static propTypes = {
         contacts:PropTypes.array.isRequired,
         onDeleteContact: PropTypes.func.isRequired
-    }
+    };
 
     state = {
         query: ""
-    }
+    };
 
     updateQuery = (query) => {
         this.setState({query : query.trim()})
-    }
+    };
+
+    clearQuery = () => {
+        this.setState({query: ''})
+    };
 
     render() {
         const {contacts, onDeleteContact} = this.props;
-        const {query} = this.state
-
+        const {query} = this.state;
 
         let showingContacts;
 
@@ -45,6 +48,14 @@ class ListContacts extends Component{
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
                 </div>
+
+                {showingContacts.length !== contacts.length && (
+                    <div className='showing-contacts'>
+                        <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+                        <button onClick={this.clearQuery}>Show all</button>
+                    </div>
+                )}
+
                 <ol className='contact-list'>
                     {showingContacts.map((contact) => (
                         <li key={contact.id} className='contact-list-item'>
