e3fe97b25bd330469bc45bad73884e4b25e0faa2
zhangshiqiu <zhangshiqiu1993@126.com>
2017-12-11 18:18:11 -0800

    search page

---
+++ myreads/src/SearchBooks.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import * as BooksAPI from "./BooksAPI";
+import {Link} from 'react-router-dom';
+
+class SearchBooks extends Component {
+    state = {
+        query: "",
+        search: []
+    }
+
+    static propsType = {
+        onAddBook : PropTypes.func.isRequired
+    }
+
+    updateQuery = (query) => {
+        BooksAPI.search(query).then((result) => {
+            if (typeof result === 'undefined' || result.hasOwnProperty("error")) {
+                this.setState({
+                    query: "",
+                    search: []
+                })
+            } else {
+                this.setState({
+                    query: query.trim(),
+                    search: result
+                })
+            }
+        });
+    };
+
+    addBook = (book, shelf) => {
+        this.props.onAddBook(book, shelf);
+        this.setState((state) => ({
+            search: state.search.filter((b) => b.id !== book.id)
+        }));
+    }
+
+
+    render() {
+        const {query, search} = this.state;
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/" className="close-search" >Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            value={query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
+                        />
+
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {search.length !== 0 && search.map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        {book.hasOwnProperty("imageLinks") && (
+                                            <div className="book-cover" style={{ width: 128, height: 180, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        )}
+                                        <div className="book-shelf-changer">
+                                            <select onChange={(e) => this.addBook(book, e.target.value)} value="none">
+                                                <option value="none" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    {book.hasOwnProperty("authors") && (
+                                        <div className="book-authors">{book.authors.join(", ")}</div>
+                                    )}
+
+                                </div>
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBooks;
