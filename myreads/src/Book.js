f070eef7d04819929beb77fd6ab78a965945822f
zhangshiqiu <zhangshiqiu1993@126.com>
2017-12-11 18:18:38 -0800

    in case of page error

--- myreads/src/Book.js
+++ myreads/src/Book.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+
 class Book extends Component {
     static propTypes = {
         books: PropTypes.array.isRequired,
@@ -22,8 +23,11 @@ class Book extends Component {
                             <li key={book.id}>
                                 <div className="book">
                                     <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 180, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        {book.hasOwnProperty("imageLinks") && (
+                                            <div className="book-cover" style={{ width: 128, height: 180, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        )}
                                         <div className="book-shelf-changer">
+
                                             <select onChange={(e) => moveBook(book, e.target.value)} value={book.shelf}>
                                                 <option value="none" disabled>Move to...</option>
                                                 <option value="currentlyReading">Currently Reading</option>
@@ -34,7 +38,9 @@ class Book extends Component {
                                         </div>
                                     </div>
                                     <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors.join(", ")}</div>
+                                    {book.hasOwnProperty("authors") && (
+                                        <div className="book-authors">{book.authors.join(", ")}</div>
+                                    )}
                                 </div>
                             </li>
                         ))}
