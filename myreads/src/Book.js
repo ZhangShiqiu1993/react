8cab56445f7cbe458f10a043fceb327699693a53
zhangshiqiu <zhangshiqiu1993@126.com>
2017-12-14 11:11:22 -0800

    re-use Book class

--- myreads/src/Book.js
+++ myreads/src/Book.js
@@ -5,46 +5,39 @@ import PropTypes from 'prop-types';
 class Book extends Component {
     static propTypes = {
         books: PropTypes.array.isRequired,
-        bookshelf: PropTypes.string.isRequired,
-        shelfTitle: PropTypes.string.isRequired,
         moveBook: PropTypes.func.isRequired
     };
 
 
     render() {
-        const {books, bookshelf, shelfTitle, moveBook} = this.props;
-        let bookList = books.filter((book) => book.shelf === bookshelf);
+        const {books, moveBook} = this.props;
         return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelfTitle}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {bookList.map((book) => (
-                            <li key={book.id}>
-                                <div className="book">
-                                    <div className="book-top">
-                                        {book.hasOwnProperty("imageLinks") && (
-                                            <div className="book-cover" style={{ width: 128, height: 180, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                        )}
-                                        <div className="book-shelf-changer">
+            <ol className="books-grid">
+                {books.length !== 0 && books.map((book) => (
+                    <li key={book.id}>
+                        <div className="book">
+                            <div className="book-top">
+                                {book.hasOwnProperty("imageLinks") && (
+                                    <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                )}
+                                <div className="book-shelf-changer">
 
-                                            <select onChange={(e) => moveBook(book, e.target.value)} value={book.shelf}>
-                                                <option value="none" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.hasOwnProperty("authors") ? book.authors.join(", ") : ""}</div>
+                                    <select onChange={(e) => moveBook(book, e.target.value)} value={ typeof book.shelf === "undefined" ? "none" : book.shelf}>
+                                        <option value="none" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
                                 </div>
-                            </li>
-                        ))}
-                    </ol>
-                </div>
-            </div>
+                            </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.hasOwnProperty("authors") ? book.authors.join(", ") : ""}</div>
+                        </div>
+                    </li>
+                ))}
+            </ol>
+
         )
     }
 }
