d7d815a3f6e24c0c030ee2409e86957db99b3c25
zhangshiqiu <zhangshiqiu1993@126.com>
2017-12-18 22:23:44 +0800

    use shelf class

--- myreads/src/ListBooks.js
+++ myreads/src/ListBooks.js
@@ -1,64 +1,41 @@
 import React, {Component} from 'react';
 import './App.css';
-import Book from "./Book";
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Shelf from "./Shelf"
 
 class ListBooks extends Component {
-    static propTypes = {
-        onMoveBook: PropTypes.func.isRequired,
-        books: PropTypes.array.isRequired
-    }
-    render() {
-        const {onMoveBook, books} = this.props;
-        let currentlyReading = books.filter((book) => book.shelf === "currentlyReading");
-        let wantToRead = books.filter((book) => book.shelf === "wantToRead");
-        let read = books.filter((book) => book.shelf === "read");
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Currently Reading</h2>
-                            <div className="bookshelf-books">
-                            <Book
-                                books={currentlyReading}
-                                moveBook={onMoveBook}/>
-                            </div>
-                        </div>
+  static propTypes = {
+    onMoveBook: PropTypes.func.isRequired,
+    books: PropTypes.array.isRequired
+  }
 
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Want to Read</h2>
-                            <div className="bookshelf-books">
-                                <Book
-                                    books={wantToRead}
-                                    moveBook={onMoveBook}/>
-                            </div>
-                        </div>
-
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Read</h2>
-                            <div className="bookshelf-books">
-                                <Book
-                                    books={read}
-                                    moveBook={onMoveBook}/>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link
-                        to="/search">
-                        Add a book
-                    </Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const {onMoveBook, books} = this.props;
+    let currentlyReading = books.filter((book) => book.shelf === "currentlyReading");
+    let wantToRead = books.filter((book) => book.shelf === "wantToRead");
+    let read = books.filter((book) => book.shelf === "read");
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Shelf books={currentlyReading} moveBook={onMoveBook} title={"Currently Reading"}/>
+            <Shelf books={wantToRead} moveBook={onMoveBook} title={"Want to Read"}/>
+            <Shelf books={read} moveBook={onMoveBook} title={"Read"}/>
+          </div>
+        </div>
+        <div className="open-search">
+          <Link
+            to="/search">
+            Add a book
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ListBooks
