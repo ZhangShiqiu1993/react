ead959daf06a8b1f1225f235408fde344dd68c09
zhangshiqiu <zhangshiqiu1993@126.com>
2017-12-23 00:50:11 +0800

    clean app

--- meal-planner/src/componments/App.js
+++ meal-planner/src/componments/App.js
@@ -1,49 +1,15 @@
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
-
+import {connect} from 'react-redux'
 class App extends Component {
-  state = {
-    calendar: null
-  };
-
-  componentDidMount () {
-    const {store} = this.props;
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  };
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }));
-
-    this.input.value = "";
-  };
 
   render() {
+    console.log('Props', this.props)
     return (
       <div>
-        <input
-          type="text"
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello World
       </div>
     )
   }
 }
 
-export default App
+export default connect()(App)
