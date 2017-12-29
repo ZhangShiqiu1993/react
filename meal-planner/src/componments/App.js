d744ab389b5f2faa8fd475cf2a52fa79ecd80ae7
zhangshiqiu <zhangshiqiu1993@126.com>
2017-12-29 18:22:17 +0800

    mapStateToProps

--- meal-planner/src/componments/App.js
+++ meal-planner/src/componments/App.js
@@ -12,4 +12,18 @@ class App extends Component {
   }
 }
 
-export default connect()(App)
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal] ? calendar[day][meal] : null;
+        return meals;
+      }, {})
+    })),
+  }
+}
+
+export default connect(mapStateToProps)(App)
