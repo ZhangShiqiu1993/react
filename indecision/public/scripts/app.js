acf108ac435212b11f3ddac906f3ee359134a87c
zhangshiqiu <zhangshiqiu1993@126.com>
2018-01-19 00:07:41 -0800

    app file

--- indecision/public/scripts/app.js
+++ indecision/public/scripts/app.js
@@ -1,10 +1,35 @@
+"use strict";
+
 console.log("running");
 
 // JSX 
 var template = React.createElement(
-    "h1",
-    { id: "xx" },
-    "Something new"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Shiqiu"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
 );
 var appRoot = document.getElementById("app");
 
--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,7 +1,16 @@
 console.log("running");
 
 // JSX 
-var template = <h1>Something new</h1>
+var template = (
+    <div>
+        <h1>Shiqiu</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+)
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
