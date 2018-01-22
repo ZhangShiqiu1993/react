fb6438d481ca91928bcd554706f2e38aef6699c2
zhangshiqiu <zhangshiqiu1993@126.com>
2018-01-22 15:24:40 -0800

    test user

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -10,7 +10,21 @@ var template = (
             <li>Item two</li>
         </ol>
     </div>
-)
+);
+
+const user = {
+    name: "Andrew",
+    age: 26,
+    location: "Philadelphia"
+}
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>
+    }
+}
+
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
