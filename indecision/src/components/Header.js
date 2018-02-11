092b68ccb2a31681e003955a59afec56d7c20271
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-11 13:38:23 -0800

    header__title

--- indecision/src/components/Header.js
+++ indecision/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = (props) => (
   <div className="header">
-    <h1>{props.title}</h1>
+    <h1 className="header__title">{props.title}</h1>
     {props.subtitle && <h2>{props.subtitle}</h2> }
   </div>
 );
