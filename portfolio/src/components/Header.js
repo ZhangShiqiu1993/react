54f9c35e9264da2f767c513b7ee73e1dfc71dda3
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-16 00:49:08 -0800

    header

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -4,9 +4,10 @@ import React from 'react';
 const Header = () => (
   <header>
     <h1>Portfolio</h1>
-    <NavLink exact={true} to="/" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    <NavLink to="/portfolio" activeClassName="is-active" exact={true}>Portfolio</NavLink>
+    <NavLink to="/contact" activeClassName="is-active" exact={true}>Contact</NavLink>
+
   </header>
 );
 
