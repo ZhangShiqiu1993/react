7563714a9274920cf835404dd50948cc566db81f
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-17 01:23:55 -0800

    add expense

--- expensify/src/redux-expensify.js
+++ expensify/src/redux-expensify.js
@@ -1,18 +1,41 @@
 import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid';
+
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
 
 const expenseReducerDefaultState = [];
+
+const expenseReducer = (state = expenseReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return state.concat(action.expense);
+    default:
+      return state;
+  }
+};
+
 const filtersReducerDefaultState = {
   text: '',
   sortBy: 'date',
   startDate: undefined,
   endDate: undefined
-};
-
-const expenseReducer = (state = expenseReducerDefaultState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
 };
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
@@ -29,7 +52,12 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(addExpense({description: 'Rent', amount: 100}));
 
 const demoState = {
   expenses: [{
