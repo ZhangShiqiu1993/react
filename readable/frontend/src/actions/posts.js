06bf737f00e69aff50b8b5e5675925d9386ce2a8
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-26 16:50:38 -0800

    post actions

--- readable/frontend/src/actions/posts.js
+++ readable/frontend/src/actions/posts.js
@@ -1,5 +1,49 @@
-const addPost = () => {};
+import uuid from 'uuid';
+import moment from 'moment';
 
-const deletePost = () => {};
+export const ADD_POST = 'ADD_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const EDIT_POST = 'EDIT_POST';
+export const LOAD_POSTS = 'LOAD_POSTS';
 
-const editPost = () => {};
+export const addPost = () => ({
+  type: ADD_POST
+});
+
+export const deletePost = () => ({
+  type: DELETE_POST
+});
+
+export const editPost = () => ({
+  type: EDIT_POST
+});
+
+export const loadPosts = (posts) => ({
+  type: LOAD_POSTS,
+  posts
+});
+
+
+const post = {
+  id: uuid(),
+  timestamp: moment(),
+  title: '',
+  body: '',
+  author: '',
+  category: ''
+};
+
+const categories = [
+  {
+    name: 'react',
+    path: 'react'
+  },
+  {
+    name: 'redux',
+    path: 'redux'
+  },
+  {
+    name: 'udacity',
+    path: 'udacity'
+  }
+]
