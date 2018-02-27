0efeac66d55d629baf0ec85a02b27cea12392756
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-26 16:48:59 -0800

    add post page

---
+++ readable/frontend/src/components/AddPostPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {addPost} from '../actions/posts';
+
+const AddPostPage = (props) => (
+  <div>
+    <h1>Add Post</h1>
+
+  </div>
+);
+
+export default AddPostPage;
