857e6112e44d2a669cf497b2eaa4c5322c6d3f15
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-27 00:02:14 -0800

    add routers

--- readable/frontend/src/App.js
+++ readable/frontend/src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header';
 import PostDashboardPage from './components/PostDashboardPage';
 import EditPostPage from './components/EditPostPage';
 import AddPostPage from './components/AddPostPage';
+import EditCommentPage from './components/EditCommentPage';
+import PostDetailPage from './components/PostDetailPage';
+import CategoryPage from './components/CategoryPage';
 import NotFoundPage from './components/NotFoundPage';
 
 class App extends Component {
@@ -15,9 +18,13 @@ class App extends Component {
           <div>
             <Header />
             <Switch>
-              <Route exact={true} path="/" component={PostDashboardPage} />
+              <Route exact path="/" component={PostDashboardPage} />
               <Route path="/create" component={AddPostPage} />
-              <Route path="/edit/:id" component={EditPostPage} />
+              <Route exact path="/:category" component={CategoryPage} />
+              <Route exact path="/:category/:id" component={PostDetailPage} />
+              <Route exact path="/posts/:id" component={EditPostPage} />
+              {/*<Route path="/posts/:id/comments" component={} />*/}
+              <Route path="/comment/:id" component={EditCommentPage} />
               <Route component={NotFoundPage} />
             </Switch>
           </div>
