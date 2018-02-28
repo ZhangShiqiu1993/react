a257d78d16315b18664664d2d1bf131ec96717cc
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-28 01:23:15 -0800

    add category link

--- readable/frontend/src/components/Header.js
+++ readable/frontend/src/components/Header.js
@@ -1,12 +1,33 @@
 import {NavLink} from 'react-router-dom';
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-const Header = () => (
-  <header>
-    <h1>Readable</h1>
-    <NavLink exact={true} to="/" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Post</NavLink>
-  </header>
-);
 
-export default Header;
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Readable</h1>
+        <NavLink exact={true} to="/" activeClassName="is-active">Dashboard </NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Post </NavLink>
+        {this.props.categories && this.props.categories.map((category) => (
+          <NavLink
+            to={`/${category.path}`}
+            key={category.name}
+            activeClassName="is-active"
+          >
+            {category.name}
+          </NavLink>
+        ))}
+      </header>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    categories: state.category.categories
+  }
+};
+
+export default connect(mapStateToProps)(Header);
