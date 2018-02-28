ca406ff4dc5db4cf39de0a4e4b7273c5c6236823
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-28 01:23:30 -0800

    add sort option

--- readable/frontend/src/components/PostDashboardPage.js
+++ readable/frontend/src/components/PostDashboardPage.js
@@ -3,6 +3,14 @@ import PostList from "./PostList";
 
 const PostDashboardPage = () => (
   <div>
+    order by
+    <select name="" id="">
+      <option value="latest-date">latest date</option>
+      <option value="earliest-date">earliest date</option>
+      <option value="highest-score">highest score</option>
+      <option value="lowest-score">lowest score</option>
+    </select>
+
     <PostList/>
 
   </div>
