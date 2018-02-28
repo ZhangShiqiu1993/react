ec079b0e484d4d674de16bb98b8171a988b3a4a2
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-28 01:22:49 -0800

    load data

--- readable/frontend/src/store/configureStore.js
+++ readable/frontend/src/store/configureStore.js
@@ -1,12 +1,21 @@
 import {createStore, combineReducers} from 'redux';
 import postReducer from '../reducers/posts';
+import categoryReducer from '../reducers/categories';
+import commentReducer from '../reducers/comments';
+import {loadCategoriesIntoStore} from "../utils/category";
+import {loadPostsIntoStore} from "../utils/post";
 
 export default () => {
   const store = createStore(
     combineReducers({
-      posts: postReducer
+      posts: postReducer,
+      category: categoryReducer,
+      comments: commentReducer
     }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  loadPostsIntoStore(store);
+  loadCategoriesIntoStore(store);
   return store;
 }
