ae5c07a70f95568c4bf4e2a4ca5e8ff5df0c66a6
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-28 01:23:51 -0800

    show posts in category page

--- readable/frontend/src/components/CategoryPage.js
+++ readable/frontend/src/components/CategoryPage.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PostDetailPage from './PostDetailPage';
 
 class CategoryPage extends Component {
   render() {
     return (
       <div>
         <h3>Category</h3>
-        <p>{this.props.match.params.category}</p>
+        <p>{this.props.posts.length}</p>
+        {this.props.posts && this.props.posts.map((post) => (
+          <PostDetailPage key={post.id} {...post}/>
+        ))}
       </div>
     )
   }
 }
 
-export default CategoryPage;
+const mapStateToProps = (state, props) => {
+  const category = props.match.params.category;
+  return {
+    posts: state.posts.filter((post) => post.category === category)
+  }
+};
+
+export default connect(mapStateToProps)(CategoryPage);
