46eb29e801422beaadf745cddd0260a2ad154be8
zhangshiqiu <zhangshiqiu1993@126.com>
2018-02-28 01:24:29 -0800

    comment actions

--- readable/frontend/src/actions/comments.js
+++ readable/frontend/src/actions/comments.js
@@ -1,5 +1,41 @@
-const addComment = () => {};
+import uuid from 'uuid';
+import moment from 'moment';
 
-const deleteComment = () => {};
 
-const editComment = () => {};
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const LOAD_COMMENTS = 'LOAD_COMMENTS';
+
+export const loadComments = (comments) => ({
+  type: LOAD_COMMENTS,
+  comments
+});
+
+export const createComment = ({body='', author='', parentID=0} = {}) => ({
+  type: ADD_COMMENT,
+  comment: {
+    id: uuid(),
+    timestamp: moment.valueOf(),
+    body,
+    author,
+    parentID
+  }
+});
+
+export const deleteComment = (id) => ({
+  type: DELETE_COMMENT,
+  id
+});
+
+export const editComment = () => ({
+  type: EDIT_COMMENT
+});
+
+const comment = {
+  id: uuid(),
+  timestamp: moment(),
+  body: '',
+  author: '',
+  parentId: uuid()
+};
