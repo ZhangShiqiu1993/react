743427e09eefccd7528bc7d3a77365fae6bfd9ac
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-01 00:11:31 -0800

    post detail page

--- readable/frontend/src/components/PostDetailPage.js
+++ readable/frontend/src/components/PostDetailPage.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {getCommentsByPost} from '../utils/api';
+import {connect} from 'react-redux';
+import {loadComments} from "../actions/comments";
 
 const PostDetailPage =
-  ({ id, timestamp, title, body, author, category, voteScore, deleted, commentCount }) => (
+({ id, timestamp, title, body, author, category, voteScore, deleted, commentCount }) => {
+  return (
     <div>
       <Link to={`/posts/${id}`}>
         <h3>{title}</h3>
       </Link>
-      <p>author - {author}</p>
-      <p>comment: {commentCount}</p>
-      <p>current score: {voteScore}</p>
       <p>body - {body}</p>
-
-      <Link to={`/${category}`}>category - {category}</Link>
+      <p>author - {author}</p>
+      <p>number of comment: {commentCount}</p>
+      <p>current score: {voteScore}</p>
+      <button>upvote</button>
+      <button>downvote</button>
+      <button>edit</button>
+      <button>delete</button>
+      {/*<p>{this.props.comments.length}</p>*/}
     </div>
   );
+}
 
-export default PostDetailPage;
+function mapDispatchToProps (dispatch, props) {
+  return {
+    getComments: () => {
+      const comments = getCommentsByPost(props.id);
+      comments.then((comments) => {
+        dispatch(loadComments(comments))
+      })
+    }
+  }
+}
+
+
+export default connect()(PostDetailPage);
