4c920bd733d4d4e159711b288c9dc3079a85f45d
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-02 18:42:33 -0800

    load comments for every post

--- readable/frontend/src/utils/category.js
+++ readable/frontend/src/utils/category.js
@@ -1,8 +1,12 @@
 import {loadCategories} from '../actions/categories';
 import {getCategories} from './api';
+import {loadCommentsIntoStore} from './comment';
 
 export const loadCategoriesIntoStore = store => {
+  console.log("loadCategoriesIntoStore");
   getCategories().then((res) => {
     store.dispatch(loadCategories(res))
+  }).then(() => {
+    loadCommentsIntoStore(store)
   })
 };
