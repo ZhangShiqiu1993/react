dbf042774cd90816fac5309e3744b20125a9e3fe
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-11 01:06:34 -0600

    load data into state

--- readable/frontend/src/components/PostForm.js
+++ readable/frontend/src/components/PostForm.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 
 class PostForm extends Component {
+  state = {
+    author: this.props ? this.props.author: '',
+    body: this.props ? this.props.body: '',
+    category: this.props ? this.props.category: '',
+    deleted: this.props ? this.props.deleted: false,
+    title: this.props ? this.props.title: '',
+    voteScore: this.props ? this.props.voteScore: 0,
+    timestamp: this.props ? moment(this.props.timestamp): moment()
+  };
+
+
   render() {
     return (
       <div>
@@ -9,3 +21,5 @@ class PostForm extends Component {
     );
   }
 }
+
+export default PostForm;
