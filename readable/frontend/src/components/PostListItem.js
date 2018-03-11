e268fc23e8f6a3dcba7a8aaf4085dc77b4eec46c
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-11 00:04:24 -0600

    post list item

--- readable/frontend/src/components/PostListItem.js
+++ readable/frontend/src/components/PostListItem.js
@@ -1,28 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 
-class PostListItem extends Component {
-  render() {
-    const {id, title, body, author, category, voteScore, deleted, commentCount} = this.props;
-    console.log(this.props);
-    return (
-      <div>
-        <Link to={`/posts/${id}`}>
-          <h3>{title}</h3>
-        </Link>
-        <p>author - {author}</p>
-        <p>comment: {commentCount}</p>
-        <p>current score: {voteScore}</p>
-        <p>body - {body}</p>
+const PostListItem =
+  ({id, title, body, author, category, voteScore, commentCount}) => (
+  <div>
+    <Link to={`/posts/${id}`}>
+      <h3>{title}</h3>
+    </Link>
+    <p>author - {author}</p>
+    <p>comment: {commentCount}</p>
+    <p>current score: {voteScore}</p>
+    <p>body - {body}</p>
 
-        <Link to={`/${category}`}>category - {category}</Link>
-        <br/>
-        <button>upvote</button>
-        <button>downvote</button>
-        <button>delete</button>
-      </div>
-    );
-  }
-}
+    <Link to={`/${category}`}>category - {category}</Link>
+  </div>
+);
 
 export default PostListItem;
