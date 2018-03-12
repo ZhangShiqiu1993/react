52e8c8ab1e48b3e66ba2ddcc18ea79d794866ffb
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-12 00:32:01 -0600

    post form

--- readable/frontend/src/components/PostForm.js
+++ readable/frontend/src/components/PostForm.js
@@ -9,16 +9,38 @@ class PostForm extends Component {
     deleted: this.props ? this.props.deleted: false,
     title: this.props ? this.props.title: '',
     voteScore: this.props ? this.props.voteScore: 0,
-    timestamp: this.props ? moment(this.props.timestamp): moment()
+    timestamp: this.props ? moment(this.props.timestamp): moment(),
+    error: ''
   };
 
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const {title, author, body, category, deleted, voteScore, timestamp} = this.state;
+    if (!title || !author || !body || !category) {
+      this.setState({error: "Please provide title, body, author and category"});
+    } else {
+      this.setState({error: ""});
+      this.props.onSubmit({
+        author, body, title, category, deleted, voteScore,
+        timestamp: timestamp.valueOf()
+      });
+    }
+  };
 
   render() {
     return (
       <div>
-        <h1>PostForm</h1>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit} className="create-contact-form">
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="Name"/>
+            <input type="text" name="email" placeholder="Email"/>
+            <button>Add Contact</button>
+          </div>
+        </form>
       </div>
-    );
+    )
   }
 }
 
