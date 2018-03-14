31f0481579fd1ebf2b52eb70e4a47a9450a4ded2
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-13 22:11:49 -0500

    wait for re-factoring

--- readable/frontend/src/utils/category.js
+++ readable/frontend/src/utils/category.js
@@ -2,10 +2,12 @@ import {loadCategories} from '../actions/categories';
 import {getCategories} from './api';
 import {loadCommentsIntoStore} from './comment';
 
+// TODO : pass dispatch instead of store
 export const loadCategoriesIntoStore = store => {
   getCategories().then((res) => {
     store.dispatch(loadCategories(res))
   }).then(() => {
+    // TODO : pass dispatch instead of store
     loadCommentsIntoStore(store);
   })
 };
