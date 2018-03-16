03155a0f293752e7623251db4ab7a3f50b13ff57
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-16 09:06:18 -0500

    remove comments loading

--- readable/frontend/src/utils/category.js
+++ readable/frontend/src/utils/category.js
@@ -1,12 +1,12 @@
 import {loadCategories} from '../actions/categories';
 import {getCategories} from './api';
-import {loadCommentsIntoStore} from './comment';
 
 export const loadCategoriesIntoStore = dispatch => {
   getCategories().then((res) => {
     dispatch(loadCategories(res))
-  }).then(() => {
-    // TODO : pass dispatch instead of store
-    loadCommentsIntoStore(dispatch);
   })
+  //   .then(() => {
+  //   // TODO : pass dispatch instead of store
+  //   loadCommentsIntoStore(dispatch);
+  // })
 };
