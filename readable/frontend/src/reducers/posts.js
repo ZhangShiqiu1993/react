1e88bbc0897ad30b084d9df7efb7b748382d2974
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-20 16:34:52 -0700

    add post

--- readable/frontend/src/reducers/posts.js
+++ readable/frontend/src/reducers/posts.js
@@ -1,10 +1,16 @@
 import {ADD_POST, EDIT_POST, DELETE_POST, LOAD_POSTS} from '../actions/posts';
-import {deletePost, editPost} from '../utils/api';
+import {deletePost, editPost, addPost} from '../utils/api';
 
 const postReducerDefaultState = [];
 
 export default (state = postReducerDefaultState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      addPost(action.post);
+      return [
+        ...state,
+        action.post
+      ];
     case LOAD_POSTS:
       return state.concat(action.posts);
     case EDIT_POST:
@@ -24,13 +30,6 @@ export default (state = postReducerDefaultState, action) => {
       // deletePost(action.post_id);
       return state.map((post) => {
         if (post.id === action.post_id) {
-          // console.log('current', post);
-          // const new_state = {
-          //   ...post,
-          //   deleted:true
-          // };
-          // console.log('new', new_state);
-          // return post;
           return {
             ...post,
             deleted: true
