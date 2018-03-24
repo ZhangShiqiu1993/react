13982e8b661a7be5f01bce74ef422a3fae57dc65
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-23 23:39:55 -0700

    change data loading

--- readable/frontend/src/utils/post.js
+++ readable/frontend/src/utils/post.js
@@ -1,8 +1,11 @@
-import {loadPosts} from '../actions/posts'
+import {loadPosts} from '../actions/posts';
+import {loadComments} from '../actions/comments';
 import {getPosts} from './api';
 
 export const loadPostsIntoStore = (dispatch) => {
   getPosts().then((res) => {
     dispatch(loadPosts(res));
+  }).then((res)=> {
+    dispatch(loadComments(res));
   });
 };
