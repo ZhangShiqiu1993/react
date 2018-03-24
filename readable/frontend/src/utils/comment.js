c384fe1edfc9f5d328bfd21da8699d221afe73d5
zhangshiqiu <zhangshiqiu1993@126.com>
2018-03-24 00:02:20 -0700

    remove original loading function

--- readable/frontend/src/utils/comment.js
+++
@@ -1,8 +0,0 @@
-import {loadComments} from '../actions/comments';
-import {getCommentsByPost} from './api';
-
-export const loadCommentsIntoStore = (dispatch, id) => {
-  getCommentsByPost(id).then((res) => {
-    dispatch(loadComments(res))
-  })
-};
