0b2d21c652d5e11d153d5721f230e8a53ac6767c
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-08 16:22:37 -0700

    add dispatch

--- readable/frontend/src/components/PostListItem.js
+++ readable/frontend/src/components/PostListItem.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {upVotePost, downVotePost} from "../actions/posts";
 
-const PostListItem =
-  ({id, title, body, author, category, voteScore, commentCount}) => (
-  <div>
-    <Link to={`/posts/${id}`}>
-      <h3>{title}</h3>
-    </Link>
-    <p>author - {author}</p>
-    <p>comment: {commentCount}</p>
-    <p>current score: {voteScore}</p>
-    <p>body - {body}</p>
+class PostListItem extends Component {
+  upvote = (e) => {
+    console.log("upvote", this.props.id);
+    this.props.dispatch(upVotePost(this.props.id))
+  };
 
-    <Link to={`/${category}`}>category - {category}</Link>
-  </div>
-);
+  downvote = (e) => {
+    console.log('downvote', this.props.id);
+    this.props.dispatch(downVotePost(this.props.id))
+  };
 
-export default PostListItem;
+  render() {
+    const {id, title, body, author, category, voteScore, commentCount} = this.props;
+    return (
+      <div>
+        <Link to={`/posts/${id}`}>
+          <h3>{title}</h3>
+        </Link>
+        <p>author - {author}</p>
+        <p>comment: {commentCount}</p>
+        <p>current score: {voteScore}</p>
+        <p>body - {body}</p>
+
+        <Link to={`/${category}`}>category - {category}</Link>
+        <button onClick={this.upvote}>upvote</button>
+        <button onClick={this.downvote}>downvote</button>
+      </div>
+    );
+  }
+}
+
+export default connect()(PostListItem);
