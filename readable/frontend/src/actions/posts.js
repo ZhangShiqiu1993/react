f6aaeee478d0f6d31561c5b64695b5830d50a049
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-08 16:22:57 -0700

    up vote score action

--- readable/frontend/src/actions/posts.js
+++ readable/frontend/src/actions/posts.js
@@ -2,6 +2,8 @@ export const ADD_POST = 'ADD_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const EDIT_POST = 'EDIT_POST';
 export const LOAD_POSTS = 'LOAD_POSTS';
+export const UP_VOTE_POST = 'UP_VOTE_POST';
+export const DOWN_VOTE_POST = 'DOWN_VOTE_POST';
 
 export const addPost = (post) => ({
   type: ADD_POST,
@@ -22,4 +24,9 @@ export const editPost = (id, updates) => ({
 export const loadPosts = (posts) => ({
   type: LOAD_POSTS,
   posts
+});
+
+export const upVotePost = (post_id) => ({
+  type: UP_VOTE_POST,
+  post_id,
 });
