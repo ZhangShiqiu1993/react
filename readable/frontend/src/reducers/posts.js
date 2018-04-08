63e56abeafada4f58878594401cc1a3f52575072
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-08 16:22:14 -0700

    up vote score reducer

--- readable/frontend/src/reducers/posts.js
+++ readable/frontend/src/reducers/posts.js
@@ -1,9 +1,20 @@
-import {ADD_POST, EDIT_POST, DELETE_POST, LOAD_POSTS} from '../actions/posts';
+import {ADD_POST, EDIT_POST, DELETE_POST, LOAD_POSTS, UP_VOTE_POST, DOWN_VOTE_POST} from '../actions/posts';
 
 const postReducerDefaultState = [];
 
 export default (state = postReducerDefaultState, action) => {
   switch (action.type) {
+    case UP_VOTE_POST:
+      return state.map((post) => {
+        if (post.id === action.post_id) {
+          return {
+            ...post,
+            voteScore: post.voteScore + 1
+          }
+        } else {
+          return post;
+        }
+      });
     case ADD_POST:
       return [
         ...state,
