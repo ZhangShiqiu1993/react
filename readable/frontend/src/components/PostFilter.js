854580d27b73d92074ea9fa5541e4930afba2425
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-09 00:14:01 -0700

    filter

---
+++ readable/frontend/src/components/PostFilter.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class PostFilter extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default connect()(PostFilter);
