c189f4b9365cf5844592819e98165bbf8de40fd6
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-10 12:14:29 -0700

    post filter class

--- readable/frontend/src/components/PostFilter.js
+++ readable/frontend/src/components/PostFilter.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {sortByScore, sortByTime} from "../actions/filters";
 
 class PostFilter extends Component {
   render() {
+    console.log('props', this.props);
     return (
       <div>
-
+        <select
+          value={this.props.filter ? this.props.filter.field : 'timestamp'}
+          onChange={(e) => {
+            if (e.target.value === 'timestamp') {
+              this.props.dispatch(sortByTime());
+            } else if (e.target.value === 'voteScore') {
+              this.props.dispatch(sortByScore());
+            }
+          }}
+        >
+          <option value="timestamp">timestamp</option>
+          <option value="voteScore">voteScore</option>
+        </select>
       </div>
     )
   }
 }
 
-export default connect()(PostFilter);
+const mapStateToProps = (state) => {
+  return {
+    filter: state.filter
+  }
+};
+
+export default connect(mapStateToProps)(PostFilter);
