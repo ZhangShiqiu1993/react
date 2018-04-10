7af867868813f6fb21f200c8a9707a8dbec8a329
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-10 12:20:35 -0700

    reduce code

--- readable/frontend/src/components/PostFilter.js
+++ readable/frontend/src/components/PostFilter.js
@@ -1,20 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {sortByScore, sortByTime} from "../actions/filters";
+import {sorting} from "../actions/filters";
 
 class PostFilter extends Component {
   render() {
-    console.log('props', this.props);
     return (
       <div>
         <select
           value={this.props.filter ? this.props.filter.field : 'timestamp'}
           onChange={(e) => {
-            if (e.target.value === 'timestamp') {
-              this.props.dispatch(sortByTime());
-            } else if (e.target.value === 'voteScore') {
-              this.props.dispatch(sortByScore());
-            }
+            this.props.dispatch(sorting(e.target.value));
           }}
         >
           <option value="timestamp">timestamp</option>
