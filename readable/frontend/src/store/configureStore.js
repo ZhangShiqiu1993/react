3c48576b53d4847ab7a20870ad13fb520ff4e1e4
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-10 12:15:20 -0700

    add filter reducer into store

--- readable/frontend/src/store/configureStore.js
+++ readable/frontend/src/store/configureStore.js
@@ -2,13 +2,15 @@ import {createStore, combineReducers} from 'redux';
 import postReducer from '../reducers/posts';
 import categoryReducer from '../reducers/categories';
 import commentReducer from '../reducers/comments';
+import filterReducer from '../reducers/filter';
 
 export default () => {
   const store = createStore(
     combineReducers({
       posts: postReducer,
       category: categoryReducer,
-      comments: commentReducer
+      comments: commentReducer,
+      filter: filterReducer
     }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
