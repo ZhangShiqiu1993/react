6ea79af8ff157b3d1fad89d8dabada6e62596112
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-10 12:20:48 -0700

    only use one sort action

--- readable/frontend/src/actions/filters.js
+++ readable/frontend/src/actions/filters.js
@@ -1,10 +1,6 @@
-export const SORT_BY_TIME = 'SORT_BY_TIME';
-export const SORT_BY_SCORE = 'SORT_BY_SCORE';
+export const SORTING = 'SORTING';
 
-export const sortByTime = () => ({
-  type:SORT_BY_TIME
-});
-
-export const sortByScore = () => ({
-  type: SORT_BY_SCORE
+export const sorting = (field) => ({
+  type:SORTING,
+  field
 });
