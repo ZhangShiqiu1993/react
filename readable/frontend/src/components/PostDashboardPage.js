b787bd90ec5b2559f0eeaf76b6db74e395954c7a
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-10 12:13:59 -0700

    use PostFilter componment

--- readable/frontend/src/components/PostDashboardPage.js
+++ readable/frontend/src/components/PostDashboardPage.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import PostList from "./PostList";
+import PostFilter from './PostFilter'
 
 const PostDashboardPage = () => (
   <div>
-    order by
-    <select name="" id="">
-      <option value="latest-date">latest date</option>
-      <option value="earliest-date">earliest date</option>
-      <option value="highest-score">highest score</option>
-      <option value="lowest-score">lowest score</option>
-    </select>
-
+    <PostFilter/>
     <PostList/>
-
   </div>
 );
 
