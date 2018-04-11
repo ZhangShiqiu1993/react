0a1751baa6a561da0b1a2c09a2e91cd23c7622dd
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-11 00:18:55 -0700

    edit comment page

--- readable/frontend/src/components/EditCommentPage.js
+++ readable/frontend/src/components/EditCommentPage.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import * as API from "../utils/api";
+import {connect} from "react-redux";
+import CommentForm from './CommentForm';
+import {deleteComment, editComment} from "../actions/comments";
 
-const EditCommentPage = (props) => (
-  <div>
-    <h1>Edit Comment Post</h1>
+const EditCommentPage = (props) => {
+  const comment = props.comment;
+  return (
+    <div>
+      <h1>Edit Comment Post</h1>
+      {!!comment && (
+        <div>
+          <CommentForm
+            parent={props.post}
+            comment={comment}
+            onSubmit={(comment) => {
+              API.editComment(props.comments.id, comment).then(() => {
+                props.dispatch(editComment(props.comment.id, comment));
+              });
+              props.history.push('/');
+            }}
+          />
+          <button onClick={() => {
+            API.deleteComment(comment.id).then(() => {
+              props.dispatch(deleteComment(comment.id));
+            });
+            props.history.push('/');
+          }}>delete</button>
+        </div>
+      )}
+    </div>
+  )
+};
 
-  </div>
-);
+const mapStateToProps = (state, props) => {
+  const comment = state.comments.find((comment) => comment.id === props.match.params.id);
+  return {
+    comment,
+    post: state.posts.find((post) => post.id === comment.parentId)
+  }
+};
 
-export default EditCommentPage;
+export default connect(mapStateToProps)(EditCommentPage);
