887deb039ecc9f5903a433155001b18b01b37b32
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-12 00:26:34 -0700

    add thunk and logger

--- readable/frontend/src/store/configureStore.js
+++ readable/frontend/src/store/configureStore.js
@@ -1,8 +1,21 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import postReducer from '../reducers/posts';
 import categoryReducer from '../reducers/categories';
 import commentReducer from '../reducers/comments';
 import filterReducer from '../reducers/filter';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+// const logger = store => next => action => {
+//   console.group(action.type)
+//   console.info('dispatching', action)
+//   let result = next(action)
+//   console.log('next state', store.getState())
+//   console.groupEnd(action.type)
+//   return result
+// }
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default () => {
   const store = createStore(
@@ -12,8 +25,9 @@ export default () => {
       comments: commentReducer,
       filter: filterReducer
     }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+      applyMiddleware(thunk,logger)
+    )
   );
-
   return store;
 }
