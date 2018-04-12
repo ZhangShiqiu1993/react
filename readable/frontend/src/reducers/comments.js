64a31e5d713777fc5a267c0ece583ceb33103f35
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-12 00:30:53 -0700

    comment reducer

--- readable/frontend/src/reducers/comments.js
+++ readable/frontend/src/reducers/comments.js
@@ -1,9 +1,58 @@
-import {ADD_COMMENT, EDIT_COMMENT, DELETE_COMMENT, LOAD_COMMENTS} from '../actions/comments';
+import {ADD_COMMENT, EDIT_COMMENT, DELETE_COMMENT, LOAD_COMMENTS, DOWN_VOTE_COMMENT, UP_VOTE_COMMENT} from '../actions/comments';
 
 const commentReducerDefaultState = [];
 
 export default (state = commentReducerDefaultState, action) => {
   switch (action.type) {
+    case ADD_COMMENT:
+      return [
+        ...state,
+        action.comment
+      ];
+    case EDIT_COMMENT:
+      return state.map((comment) => {
+        if (comment.id === action.id) {
+          return {
+            ...comment,
+            ...action.updates
+          };
+        } else {
+          return comment;
+        }
+      });
+    case DELETE_COMMENT:
+      return state.map((comment) => {
+        if (comment.id === action.id) {
+          return {
+            ...comment,
+            deleted: true
+          };
+        } else {
+          return comment;
+        }
+      });
+    case UP_VOTE_COMMENT:
+      return state.map((comment) => {
+        if (comment.id === action.id) {
+          return {
+            ...comment,
+            voteScore: comment.voteScore + 1
+          }
+        } else {
+          return comment;
+        }
+      });
+    case DOWN_VOTE_COMMENT:
+      return state.map((comment) => {
+        if (comment.id === action.id) {
+          return {
+            ...comment,
+            voteScore: comment.voteScore - 1
+          }
+        } else {
+          return comment;
+        }
+      });
     case LOAD_COMMENTS:
       return state.concat(action.comments);
     default:
