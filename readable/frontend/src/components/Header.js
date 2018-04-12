1d8eb33954e2507bb4025a317b0de36c75aa2a4c
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-12 00:27:44 -0700

    use breadcrumb and icon from antd

--- readable/frontend/src/components/Header.js
+++ readable/frontend/src/components/Header.js
@@ -1,24 +1,41 @@
 import {NavLink} from 'react-router-dom';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-
+import { Breadcrumb, Icon } from 'antd';
 
 class Header extends Component {
   render() {
     return (
       <header>
         <h1>Readable</h1>
-        <NavLink exact={true} to="/" activeClassName="is-active">Dashboard </NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Post </NavLink>
-        {this.props.categories && this.props.categories.map((category) => (
-          <NavLink
-            to={`/${category.path}`}
-            key={category.name}
-            activeClassName="is-active"
-          >
-            {category.name}
-          </NavLink>
-        ))}
+        <Breadcrumb>
+          <Breadcrumb.Item>
+            <NavLink exact={true} to="/" activeClassName="is-active">
+              <Icon type="home"/>
+              Dashboard
+            </NavLink>
+          </Breadcrumb.Item>
+
+          <Breadcrumb.Item>
+            <NavLink to="/create" activeClassName="is-active">
+              <Icon type="form" />
+              Create Post
+            </NavLink>
+          </Breadcrumb.Item>
+
+          {this.props.categories && this.props.categories.map((category) => (
+            <Breadcrumb.Item key={category.name}>
+              <NavLink
+                to={`/${category.path}`}
+                key={category.name}
+                activeClassName="is-active"
+              >
+                <Icon type="tags" />
+                {category.name}
+              </NavLink>
+            </Breadcrumb.Item>
+          ))}
+        </Breadcrumb>
       </header>
     )
   }
