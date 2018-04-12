b90bd38972838ec0719209a3df2cfdfc82c85cc1
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-12 00:28:52 -0700

    use logger and remove console print

--- readable/frontend/src/index.js
+++ readable/frontend/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 
 const store = configureStore();
-
-store.subscribe(() => {
-  console.log("store: ", store.getState());
-});
+//
+// store.subscribe(() => {
+//   console.log("store: ", store.getState());
+// });
 
 const jsx = (
   <Provider store={store}>
