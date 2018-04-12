c3e55215923c2717e831f4fe4ce7f7b156a59b6d
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-12 00:28:28 -0700

    add css

--- readable/frontend/src/App.js
+++ readable/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import './App.css';
 import Header from './components/Header';
 import PostDashboardPage from './components/PostDashboardPage';
 import EditPostPage from './components/EditPostPage';
