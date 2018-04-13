6057d19bef682809809a57aa436217bf412b8fe7
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-13 00:19:42 -0700

    apply antd on comment list

--- readable/frontend/src/components/CommentList.js
+++ readable/frontend/src/components/CommentList.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Comment from './Comment';
+import { Row, Col} from 'antd';
 
 const CommentList = (props) => (
   <div>
-    <h3>comment</h3>
-    {props.comments.map((comment) => {
-      return <Comment key={comment.id} {...comment}/>
-    })}
+    {props.comments.length !== 0 && (
+    <div>
+      <div className="space" />
+      <Row type="flex" justify="center">
+        <Col className="gutter-row" span={2}>
+          <div className="gutter-box">Author</div>
+        </Col>
+        <Col className="gutter-row" span={4}>
+          <div className="gutter-box">Comment</div>
+        </Col>
+        <Col className="gutter-row" span={2}>
+          <div className="gutter-box">Vote Score</div>
+        </Col>
+        <Col className="gutter-row" span={8}>
+          <div className="gutter-box">Actions</div>
+        </Col>
+      </Row>
+      {props.comments.map((comment) => {
+        return <Comment key={comment.id} {...comment}/>
+      })}
+    </div>
+    )}
   </div>
 );
 
