cb5315d3991ce87a2f726919dab5da9376facf15
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-13 00:19:57 -0700

    edit comment page

--- readable/frontend/src/components/EditCommentPage.js
+++ readable/frontend/src/components/EditCommentPage.js
@@ -3,30 +3,35 @@ import * as API from "../utils/api";
 import {connect} from "react-redux";
 import CommentForm from './CommentForm';
 import {deleteComment, editComment} from "../actions/comments";
+import {Button} from 'antd';
 
 const EditCommentPage = (props) => {
-  const comment = props.comment;
+  const {comment, post} = props;
   return (
     <div>
-      <h1>Edit Comment Post</h1>
       {!!comment && (
         <div>
+          <h3>Edit Comment or
+            <Button
+              type={"danger"}
+              onClick={() => {
+                API.deleteComment(comment.id).then(() => {
+                  props.dispatch(deleteComment(comment.id));
+                });
+                props.history.push('/');
+              }}
+            >delete</Button>
+          </h3>
           <CommentForm
             parent={props.post}
             comment={comment}
             onSubmit={(comment) => {
-              API.editComment(props.comments.id, comment).then(() => {
+              API.editComment(props.comment.id, comment).then(() => {
                 props.dispatch(editComment(props.comment.id, comment));
               });
-              props.history.push('/');
+              props.history.push(`/${post.category}/${post.id}`);
             }}
           />
-          <button onClick={() => {
-            API.deleteComment(comment.id).then(() => {
-              props.dispatch(deleteComment(comment.id));
-            });
-            props.history.push('/');
-          }}>delete</button>
         </div>
       )}
     </div>
