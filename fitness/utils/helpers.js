f63614a637eed10df60815be908247d32a3eb370
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-14 23:41:47 -0700

    info object

--- fitness/utils/helpers.js
+++ fitness/utils/helpers.js
@@ -4,7 +4,98 @@ import {FontAwesome, MaterialIcons, MaterialCommunityIcons} from '@expo/vector-i
 import {white} from "./colors";
 
 export function getMetricMetaInfo (metric) {
-  const info = {}
+  const info = {
+    run: {
+      displayName: 'Run',
+      max: 50,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialIcons
+              name='directions-run'
+              color={'black'}
+              size={35}
+            />
+          </View>
+        )
+      }
+    },
+    bike:{
+      displayName: 'Bike',
+      max: 100,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialIcons
+              name='bike'
+              color={'black'}
+              size={32}
+            />
+          </View>
+        )
+      }
+    },
+    swim:{
+      displayName: 'Swim',
+      max: 9900,
+      unit: 'meters',
+      step: 100,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialIcons
+              name='swim'
+              color={'black'}
+              size={35}
+            />
+          </View>
+        )
+      }
+    },
+    sleep:{
+      displayName: 'Sleep',
+      max: 24,
+      unit: 'hours',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+          <View>
+            <MaterialIcons
+              name='bed'
+              color={'black'}
+              size={30}
+            />
+          </View>
+        )
+      }
+    },
+    eat:{
+      displayName: 'Eat',
+      max: 10,
+      unit: 'rating',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+          <View>
+            <MaterialIcons
+              name='food'
+              color={'black'}
+              size={35}
+            />
+          </View>
+        )
+      }
+    }
+  };
 
   return typeof metric === 'undefined'
     ? info
