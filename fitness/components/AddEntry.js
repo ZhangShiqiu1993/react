832483bb3a02c5c2f33bca074b0b1921d9d80168
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-15 14:24:54 -0700

    increment

--- fitness/components/AddEntry.js
+++ fitness/components/AddEntry.js
@@ -3,6 +3,27 @@ import { View } from 'react-native'
 import {getMetricMetaInfo} from "../utils/helpers"
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  }
+
+  increment = (metric) => {
+    const {max, step} = getMetricMetaInfo(metric)
+
+    this.setState((state) => {
+      const count = state[metric] + step
+
+      return {
+        ...state,
+        [metric]:Math.min(max, count)
+      }
+    })
+  }
+
   render() {
     return (
       <View>
