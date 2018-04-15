02c879207fbd699bc0bab494c26d37015c054ccc
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-14 23:41:37 -0700

    clean code

--- fitness/App.js
+++ fitness/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import { View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Ionicons name='ios-pizza' color='red' size={100}/>
+      <View>
+
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
