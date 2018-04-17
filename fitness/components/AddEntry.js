7669187174b3c76610e56d755dcd159c26568d54
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-17 11:55:06 -0700

    use redux to manage state

--- fitness/components/AddEntry.js
+++ fitness/components/AddEntry.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import { View,TouchableOpacity, Text } from 'react-native';
-import {getMetricMetaInfo, timeToString} from "../utils/helpers";
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue
+} from "../utils/helpers";
 import MySlider from "./MySlider";
 import MySteppers from "./MySteppers";
 import DateHeader from './DateHeader';
 import { Ionicons } from '@expo/vector-icons';
 import TextButton from './TextButton';
 import {submitEntry, removeEntry} from "../utils/api";
+import { connect } from 'react-redux';
+import {addEntry} from "../actions";
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -18,7 +24,7 @@ function SubmitBtn ({ onPress }) {
   )
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -52,28 +58,27 @@ export default class AddEntry extends Component {
   };
 
   slide = (metric, value) => {
-    this.setState({[metric]: value})
+    this.setState({[metric]: value});
   };
 
   submit = () => {
     const key = timeToString();
     const entry = this.state;
+    this.props.dispatch(addEntry({[key]: entry}));
 
-    // Update Redux
-    this.setState(() => ({run: 0, bike: 0, swim: 0, sleep: 0, eat: 0}));
+    this.setState({run: 0, bike: 0, swim: 0, sleep: 0, eat: 0});
 
     // Navigate to home
 
-    submitEntry({key, entry})
+    submitEntry({key, entry});
 
   //  clear local notification
-
   };
 
   reset = () => {
     const key = timeToString();
 
-  //  update redux
+    this.props.dispatch(addEntry({[key]: getDailyReminderValue()}));
 
   //  route to home
 
@@ -102,7 +107,7 @@ export default class AddEntry extends Component {
 
     return (
       <View>
-        <DateHeader date={(new Date().toLocaleDateString())}/>
+        <DateHeader date={(new Date()).toLocaleDateString()}/>
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key];
           const value = this.state[key];
@@ -132,3 +137,12 @@ export default class AddEntry extends Component {
     )
   }
 }
+
+function mapStateToProps (state) {
+  const key = timeToString();
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+  }
+}
+
+export default connect(mapStateToProps)(AddEntry);
