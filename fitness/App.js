8da437d7bec5a6127acf6bf393b6192336a213b4
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-17 11:55:18 -0700

    use redux and logger

--- fitness/App.js
+++ fitness/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import { View } from 'react-native'
-import AddEntry from './components/AddEntry'
+import React from 'react';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducers';
+import logger from 'redux-logger';
 
 export default class App extends React.Component {
   render() {
+    const store = createStore(
+      reducer,
+      applyMiddleware(logger)
+    );
     return (
-      <View>
-        <AddEntry />
-      </View>
+      <Provider store={store}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
     )
   }
 }
