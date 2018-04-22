4ecef34bbd530730d546a91212c69a30d614eeaa
zhangshiqiu <zhangshiqiu1993@126.com>
2018-04-22 11:18:30 -0700

    styles

--- fitness/components/AddEntry.js
+++ fitness/components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View,TouchableOpacity, Text } from 'react-native';
+import { View,TouchableOpacity, Text, StyleSheet } from 'react-native';
 import {
   getMetricMetaInfo,
   timeToString,
@@ -13,6 +13,7 @@ import TextButton from './TextButton';
 import {submitEntry, removeEntry} from "../utils/api";
 import { connect } from 'react-redux';
 import {addEntry} from "../actions";
+import {purple, white} from "../utils/colors";
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -137,6 +138,50 @@ class AddEntry extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white,
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  }
+});
 
 function mapStateToProps (state) {
   const key = timeToString();
