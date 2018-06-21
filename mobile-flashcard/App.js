70f8a841815bfabb937d7d8df696074fdba83579
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-20 20:27:12 -0700

    remove add deck

--- mobile-flashcard/App.js
+++ mobile-flashcard/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'
-import AddDeck from './components/AddDeck'
+import DeckView from './components/DeckView'
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View>
-				<AddDeck/>
+				<DeckView/>
 			</View>
 		);
 	}
