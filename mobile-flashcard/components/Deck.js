a18e8df19e525f6b98eb481c2a8dd88035c383ba
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-22 10:34:10 -0700

    deck

--- mobile-flashcard/components/Deck.js
+++ mobile-flashcard/components/Deck.js
@@ -1,17 +1,27 @@
 import React, {Component} from 'react'
 import {View, Text, TouchableOpacity} from 'react-native'
 
-export default class Deck extends Component {
-	handlePress = () => {
-		console.log("click")
+class Deck extends Component {
+	static navigationOptions = ({navigation}) => {
+		const {title} = navigation.state.params.item
+		return {
+			title
+		}
 	}
 
+
 	render() {
+		const item = this.props.navigation.state.params.item
 		return (
-			<TouchableOpacity onPress={this.handlePress}>
-				<Text>{this.props.title}</Text>
-				<Text>{this.props.questions.length}</Text>
-			</TouchableOpacity>
+			<View>
+				<Text>{item.title}</Text>
+				<Text>{item.questions.length}</Text>
+				<TouchableOpacity>Add Card</TouchableOpacity>
+				<TouchableOpacity>Start Quiz</TouchableOpacity>
+			</View>
 		)
 	}
 }
+
+
+export default Deck
