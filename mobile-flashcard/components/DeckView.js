b5cbeca02cd201600b8a039c3ec99c93b67840f4
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-21 22:35:04 -0700

    deck view

--- mobile-flashcard/components/DeckView.js
+++ mobile-flashcard/components/DeckView.js
@@ -2,28 +2,14 @@ import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 
 
-import {getDecks} from "../utils/helpers";
-import Deck from './Deck'
-
 export default class DeckView extends Component {
-	// static propTypes = {
-	// 	title: PropTypes.string.isRequired,
-	// 	questions: PropTypes.array.isRequired
-	// }
-
-	state = {
-		title: '',
-		questions: []
-	}
-
-
 	render() {
-		const decks = getDecks();
 		return (
 			<View>
-				{Object.keys(decks).map((key) => (
-					<Deck key={key} deck={decks[key]}/>
-				))}
+				<Text>{this.props.title}</Text>
+				<Text></Text>
+				<TouchableOpacity>Add Card</TouchableOpacity>
+				<TouchableOpacity>Start Quiz</TouchableOpacity>
 			</View>
 		)
 	}
