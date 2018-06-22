bfa8559789de453572d4b1cde6894b679c319c6b
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-21 22:36:03 -0700

    add card

---
+++ mobile-flashcard/components/NewQuestionView.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+
+export default class NewQuestionView extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Add Card</Text>
+			</View>
+		)
+	}
+}
