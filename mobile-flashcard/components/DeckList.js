e95c14a88d4310af7eab2060ed24518e1bd48ece
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-22 19:05:00 -0700

    decklist

---
+++ mobile-flashcard/components/DeckList.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import {View, Text, FlatList, TouchableOpacity} from 'react-native'
+
+
+import {getDecks} from "../utils/helpers";
+
+export default class DeckList extends Component {
+	renderItem = ({item}) => {
+		return (
+			<View>
+				<TouchableOpacity onPress={() => {
+					this.props.navigation.navigate(
+						'Deck',
+						{item: item}
+					)
+				}}>
+					<Text>{item.title}</Text>
+					<Text>{item.questions.length}</Text>
+				</TouchableOpacity>
+			</View>
+		)
+	}
+
+
+	render() {
+		const decks = getDecks();
+		return (
+			<View>
+				<FlatList
+					data={Object.values(decks)}
+					renderItem={this.renderItem}
+					keyExtractor={({title}) => title}
+				/>
+			</View>
+		)
+	}
+}
