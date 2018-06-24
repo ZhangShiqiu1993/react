6d1fa6d4f87d577ea7c0e53c2af61658383ff445
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-24 00:33:20 -0700

    app

--- mobile-flashcard/App.js
+++ mobile-flashcard/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { View} from 'react-native';
 import {Provider} from 'react-redux'
+import {createStore} from 'redux';
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import {lightBlue} from "./utils/colors";
-import configureStore from './store/configureStore'
 import {MainNavigator} from "./components/Navigator";
 import MyStatusBar from './components/MyStatusBar'
+import reducer from './reducers'
 
 
 export default class App extends React.Component {
 	render() {
 		return (
-			<Provider store={configureStore()}>
+			<Provider store={createStore(reducer)}>
 				<View style={{flex:1}}>
 					<MyStatusBar backgroundColor={lightBlue} barStyle="light-content" />
 					<MainNavigator />
 				</View>
 			</Provider>
-
 		);
 	}
 }
