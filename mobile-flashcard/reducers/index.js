1c556da9607820c9bc073f295f960921dcc54961
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-23 18:28:15 -0700

    reducer

--- mobile-flashcard/reducers/index.js
+++ mobile-flashcard/reducers/index.js
@@ -1,15 +1,53 @@
-import {RECEIVE_ENTRIES, ADD_ENTRY} from "../actions/index";
+import {ADD_DECK, ADD_CARD} from "../actions/index";
 
-function entries(state={}, action) {
+export const initState = {
+	React: {
+		title: 'React',
+		questions: [
+			{
+				question: 'What is React?',
+				answer: 'A library for managing user interfaces'
+			},
+			{
+				question: 'Where do you make Ajax requests in React?',
+				answer: 'The componentDidMount lifecycle event'
+			}
+		]
+	},
+	JavaScript: {
+		title: 'JavaScript',
+		questions: [
+			{
+				question: 'What is a closure?',
+				answer: 'The combination of a function and the lexical environment within which that function was declared.'
+			}
+		]
+	}
+}
+
+function entries(state=initState, action) {
 	switch (action.type) {
-		case ADD_ENTRY:
+		case ADD_DECK:
 			return {
 				...state,
-				...action.entry
+				...action.deck
 			}
+		case ADD_CARD:
+			return state.map((deck) => {
+				if (deck.title === action.deck.title) {
+					return {
+						...deck,
+						questions: deck.questions.concat(action.card)
+					};
+				} else {
+					return deck;
+				}
+			});
 		default:
 			return state
 	}
 }
+
+
 
 export default entries
