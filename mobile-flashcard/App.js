4dbea9ac9160d53fd625edfaa4c6d69a2d0818c8
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-23 18:28:46 -0700

    remove navigator and status bar

--- mobile-flashcard/App.js
+++ mobile-flashcard/App.js
@@ -1,82 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
-import {createBottomTabNavigator, createStackNavigator, DrawerNavigator} from 'react-navigation'
+import { View} from 'react-native';
+import {Provider} from 'react-redux'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
-import DeckListView from './components/DeckListView'
-import NewDeckView from './components/NewDeckView'
-import {purple, white, lightBlue} from "./utils/colors";
-import { Constants } from 'expo';
-import Deck from "./components/Deck";
-
-function MyStatusBar ({backgroundColor, ...props}) {
-	return (
-		<View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-			<StatusBar translucent backgroundColor={backgroundColor} {...props} />
-		</View>
-	)
-}
-
-const Tabs = createBottomTabNavigator({
-	DeckView: {
-		screen: DeckListView,
-		navigationOptions: {
-			tabBarLabel: "Deck",
-			tabBarIcon: ({tintColor}) => <FontAwesome name={'home'} size={30} color={tintColor} />
-		}
-	},
-	NewDeckView: {
-		screen: NewDeckView,
-		navigationOptions: {
-			tabBarLabel: "add new deck",
-			tabBarIcon: ({tintColor}) => <FontAwesome name={'dashboard'} size={30} color={tintColor} />
-		}
-	}
-},{
-	navigationOptions: {
-		header: null
-	},
-	tabBarOptions: {
-		activeTintColor: Platform.OS === 'ios' ? purple : white,
-		style: {
-			height: 56,
-			backgroundColor: Platform.OS === 'ios' ? white : purple,
-			shadowColor: 'rgba(0, 0, 0, 0.24)',
-			shadowOffset: {
-				width: 0,
-				height: 3
-			},
-			shadowRadius: 6,
-			shadowOpacity: 1
-		}
-	}
-})
-
-const MainNavigator = createStackNavigator({
-	Home: {
-		screen: Tabs,
-		navigationOptions: {
-			title: "FlashCards",
-		}
-	},
-	Deck: {
-		screen: Deck,
-		navigationOptions: {
-			headerTintColor: 'white',
-			headerStyle: {
-				backgroundColor: lightBlue
-			}
-		}
-	}
-});
+import {lightBlue} from "./utils/colors";
+import configureStore from './store/configureStore'
+import {MainNavigator} from "./components/Navigator";
+import MyStatusBar from './components/MyStatusBar'
 
 
 export default class App extends React.Component {
 	render() {
 		return (
-			<View style={{flex:1}}>
-				<MyStatusBar backgroundColor={lightBlue} barStyle="light-content" />
-				<MainNavigator />
-			</View>
+			<Provider store={configureStore()}>
+				<View style={{flex:1}}>
+					<MyStatusBar backgroundColor={lightBlue} barStyle="light-content" />
+					<MainNavigator />
+				</View>
+			</Provider>
+
 		);
 	}
 }
