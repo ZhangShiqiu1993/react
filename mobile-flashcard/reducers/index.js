33bf614902b970b3db7811b91e2830fe1a9afac7
zhangshiqiu <zhangshiqiu1993@126.com>
2018-06-24 00:32:09 -0700

    reducer

--- mobile-flashcard/reducers/index.js
+++ mobile-flashcard/reducers/index.js
@@ -1,48 +1,27 @@
-import {ADD_DECK, ADD_CARD} from "../actions/index";
+import {ADD_DECK, ADD_CARD, LOAD_DECKS} from "../actions/index";
 
-export const initState = {
-	React: {
-		title: 'React',
-		questions: [
-			{
-				question: 'What is React?',
-				answer: 'A library for managing user interfaces'
-			},
-			{
-				question: 'Where do you make Ajax requests in React?',
-				answer: 'The componentDidMount lifecycle event'
-			}
-		]
-	},
-	JavaScript: {
-		title: 'JavaScript',
-		questions: [
-			{
-				question: 'What is a closure?',
-				answer: 'The combination of a function and the lexical environment within which that function was declared.'
-			}
-		]
-	}
-}
-
-function entries(state=initState, action) {
+function entries(state={}, action) {
 	switch (action.type) {
+		case LOAD_DECKS:
+			return {
+				...state,
+				...action.decks
+			}
 		case ADD_DECK:
 			return {
 				...state,
 				...action.deck
 			}
 		case ADD_CARD:
-			return state.map((deck) => {
-				if (deck.title === action.deck.title) {
-					return {
-						...deck,
-						questions: deck.questions.concat(action.card)
-					};
-				} else {
-					return deck;
+			const deck = state[action.title]
+			return {
+				...state,
+				[action.title]: {
+					...deck,
+					questions: deck.questions.concat(action.card)
 				}
-			});
+			}
+
 		default:
 			return state
 	}
